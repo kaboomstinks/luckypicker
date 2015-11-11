f1fc895e37246974b9001da4f8725656dca2ccf5
Monching <ramondeguia@outlook.com>
2015-11-11 19:03:27 +0800

    add a way to remove every number in the array and form it as a whole string

--- lotto.js
+++ lotto.js
@@ -6,7 +6,7 @@ app.controller('lottoCtrl', function($scope){
 	
 	$scope.pick = function(){
 		var selected = document.getElementsByName('l_variant')[0].value;	// get value of selected select tag
-		var rnd_number, i=1, checker, w_array = [], l_array = [];			
+		var rnd_number, checker, lotto_num = '', i=1, l_array = [];			
 		
 		while(i<=6){
 			rnd_number = Math.floor((Math.random() * selected) + 1);		// produce a random number base on the selected lotto variant
@@ -17,7 +17,13 @@ app.controller('lottoCtrl', function($scope){
 				i++;
 			}
 		}
-		$scope.l_numbers = l_array;
+		
+		for(i=1; i<=6; i++){
+			lotto_num += l_array.pop() + ',';								// remove all the numbers from being inside an array
+		}
+		
+		lotto_num = lotto_num.substring(0, lotto_num.length - 1);			// remove comma on the last part of the string
+		$scope.l_numbers = lotto_num;
 		$scope.b_disable = false;
 	}						
 });
